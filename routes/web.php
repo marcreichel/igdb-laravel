b2f62b79ab81d964f4ca730347426dcf0784d623
Marc Reichel <marc.reichel@artemeon.de>
2023-11-01 23:05:11 +0100

    🎨 Fix code style

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ use MarcReichel\IGDBLaravel\Models\Webhook;
 /**
  * @deprecated
  */
-Route::post('{model}/{method}', static function (Request $request) {
-    return Webhook::handle($request);
-});
+Route::post('{model}/{method}', static fn (Request $request) => Webhook::handle($request));
 
-Route::post(substr(md5(config('igdb.credentials.client_id')), 0, 8) . '/{model}/{method}', static function (Request $request) {
-    return Webhook::handle($request);
-})->name('handle-igdb-webhook');
+Route::post(substr(md5(config('igdb.credentials.client_id')), 0, 8) . '/{model}/{method}', static fn (Request $request) => Webhook::handle($request))->name('handle-igdb-webhook');
