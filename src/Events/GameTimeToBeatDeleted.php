eea4acf97081fbca7b61ddbbbffe8ec8e4717372
Marc Reichel <marc.reichel@artemeon.de>
2025-02-16 14:47:40 +0100

    #98 fix code style

--- src/Events/GameTimeToBeatDeleted.php
+++ src/Events/GameTimeToBeatDeleted.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace MarcReichel\IGDBLaravel\Events;
 
-use MarcReichel\IGDBLaravel\Events\GameTimeToBeatCreated;
-
 class GameTimeToBeatDeleted extends GameTimeToBeatCreated
 {
 }
